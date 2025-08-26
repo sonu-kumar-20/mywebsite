5b9bcb9e9db09b8ede1567f80c11f06257b88e12
Sonu Kumar <523410024@nitkkr.ac.in>
2025-08-27 00:20:09 +0530

    Updated Contact.jsx with social icons style

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,29 +1,29 @@
 import './Contact.css'
 import React from 'react'
 import myimage from "../../assets/myimage.png"
+
 const About = () => {
   return (
     <div id='contact'>
       <div className="leftcontact">
-        <img src={myimage} alt="" />
+        <img src={myimage} alt="My profile" />
       </div>
 
       <div className="rightcontact">
-         
         <form action="https://formspree.io/f/meozljgd" method='POST'>
-        <h1>Message me for any Website service</h1>
-          <input name='username' type="text"  placeholder='Enter your Name' required />
+          <h1>Message me for any Website service</h1>
 
-          <input type="email" name='email' placeholder='Enter Email' required  />
-          <input type="number" name='email' placeholder='Enter Phone Number' required  />
-
+          <input name='username' type="text" placeholder='Enter your Name' required />
+          <input type="email" name='email' placeholder='Enter Email' required />
+          <input type="number" name='phone' placeholder='Enter Phone Number' required />
+          
           <textarea name="message" id="textarea" placeholder='Type your message' required></textarea>
 
-          <input type="submit" id='btn' value="send  " />
+          <input type="submit" id='btn' value="Send" />
         </form>
-
-        
       </div>
+
+     
     </div>
   )
 }
